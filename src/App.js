ef53dfe1bb5a7461ee4bd3bbfb21fb18bdc0a532
Wai Yan Min <waiyanmin@yomafleet.com>
2020-11-15 23:33:59 +0630

    feat: add action

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	  HELLO WORLD by OpsLAB
+	  HELLO GitHub Action by OpsLAB
         </p>
         <a
           className="App-link"
